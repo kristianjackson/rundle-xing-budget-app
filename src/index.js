3cca4ea5be8a3a5e3f2bca1b7a6bda7518a467d4
Kristian Jackson <kristian.jackson@gmail.com>
2022-08-29 11:21:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -114,6 +114,7 @@ function loadMessages() {
 // This first saves the image in Firebase storage.
 async function saveImageMessage(file) {
   try {
+    // 1 - We add a message with a loading icon that will get updated with the shared image.
     const messageRef = await addDoc(collection(getFirestore(), 'messages'), {
       name: getUserName(),
       imageUrl: LOADING_IMAGE_URL,
@@ -121,13 +122,16 @@ async function saveImageMessage(file) {
       timestamp: serverTimestamp()
     });
 
+    // 2 - Upload the image to Cloud Storage.
     const filePath = `${getAuth().currentUser.uid}/${messageRef.id}/${file.name}`;
     const newImageRef = ref(getStorage(), filePath);
     const fileSnapshot = await uploadBytesResumable(newImageRef, file);
-
+    
+    // 3 - Generate a public URL for the file.
     const publicImageUrl = await getDownloadURL(newImageRef);
 
-    await updateDoc(messageRef, {
+    // 4 - Update the chat message placeholder with the image's URL.
+    await updateDoc(messageRef,{
       imageUrl: publicImageUrl,
       storageUri: fileSnapshot.metadata.fullPath
     });
